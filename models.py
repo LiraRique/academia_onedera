97a1f523d48263b928ba01ad47bfb9d5be69a3b5
unknown <guilherme.2010.guilherme@gmail.com>
2021-02-02 15:23:37 -0300

    123651516

--- models.py
+++
@@ -1,7 +0,0 @@
-class Usuario:
-    def __init__(self, nome_usuario, email_usuario, dt_cadastro, senha_aplicacao,  id_usuario=None):
-        self.id_usuario = id_usuario
-        self.nome_usuario = nome_usuario
-        self.email_usuario = email_usuario
-        self.dt_cadastro = dt_cadastro
-        self.senha_aplicacao = senha_aplicacao
